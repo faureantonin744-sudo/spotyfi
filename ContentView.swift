f3d8685e39014b39f1597b82ab2e8cacf72c5063
faureantonin744-sudo <faureantonin744@gmail.com>
2026-05-01 03:43:39 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -2,51 +2,27 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var isPlaying = false
+    @State private var playing = false
 
     var body: some View {
-
         ZStack {
-
-            // 🌐 NAVIDROME SERVER
             WebView(url: URL(string: "http://192.168.1.163:4533")!)
                 .ignoresSafeArea()
 
-            // 🎌 MANGA OVERLAY
             VStack {
-
                 Spacer()
 
-                HStack {
-
-                    Spacer()
-
-                    VStack(spacing: 15) {
-
-                        Button(action: {
-                            isPlaying.toggle()
-                        }) {
-                            Image(systemName: isPlaying ? "pause.fill" : "play.fill")
-                                .font(.title2)
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(.black.opacity(0.6))
-                                .clipShape(Circle())
-                                .shadow(color: .pink, radius: 10)
-                        }
-
-                        Button(action: {
-                            // futur skip
-                        }) {
-                            Image(systemName: "forward.fill")
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(.black.opacity(0.6))
-                                .clipShape(Circle())
-                        }
-                    }
-                    .padding()
+                Button {
+                    playing.toggle()
+                } label: {
+                    Image(systemName: playing ? "pause.fill" : "play.fill")
+                        .font(.system(size: 44))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.black.opacity(0.6))
+                        .clipShape(Circle())
                 }
+                .padding(.bottom, 30)
             }
         }
     }
